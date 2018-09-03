1092fd21cb516d19dbda43fde4365ca151af7291
Tracy Herbert <tracynherbert@gmail.com>
2018-09-03 09:35:29 -0500

    not working

--- routes/home.js
+++ routes/home.js
@@ -4,28 +4,27 @@ let express = require('express'),
     bodyParser = require('body-parser');
 
 router.get('/home', (req, res) => {
-    console.log('id:' + req.user.id);
-    // 
-    let students = [];
-    db.student.findAll({
-            // include: [{all: true}],
-            attributes: ['id', 'firstName', 'lastName', 'phoneNumber', 'email', 'instrument', 'parentname1', 'parentname2', 'parentPhoneNumber', 'parentEmail', 'address'],
-            // where: {
-            //     teacherId: req.user.id
-            // },
-            order: ['lastName']
-        })
-        .then((results) => {
-            for (let i = 0; i < results.length; i++) {
-                // students.push(results[i].dataValues);
-            }
-        }).then(function (results) {
-            console.log("rendering" + students);
+    console.log('ID IS:' + req.user.id);
+    
+    // let students = [];
+    // db.student.findAll({
+    //         // include: [{all: true}],
+    //         attributes: ['id', 'firstName', 'lastName', 'phoneNumber', 'email', 'instrument', 'parentname1', 'parentname2', 'parentPhoneNumber', 'parentEmail', 'address'],
+    //         // where: {
+    //         //     teacherId: req.user.id
+    //         // },
+    //         order: ['lastName']
+        // }).then((results) => {
+        //     for (let i = 0; i < results.length; i++) {
+        //         // students.push(results[i].dataValues);
+        //     }
+        // }).then(function (results) {
+            // console.log("rendering" + students);
             res.render('home', {
-                students: students,
+                // students: students,
                 page: 'home'
             });
         });
-});
+// });
 
 module.exports = router;
