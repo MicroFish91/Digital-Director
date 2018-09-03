525b660a488bb52ba4ba2a8ae3e6d2c1c856c42d
Tracy Herbert <tracynherbert@gmail.com>
2018-09-03 10:19:36 -0500

    fixed bug

--- routes/home.js
+++ routes/home.js
@@ -3,23 +3,20 @@ let express = require('express'),
     db = require('../models'),
     bodyParser = require('body-parser');
 
-router.get('/home', (req, res) => {
-    console.log('ID IS:' + req.user.id);
-    
+router.get('/home', (req, res) => {    
     let students = [];
     db.student.findAll({
-            // include: [{all: true}],
+            include: [{all: true}],
             attributes: ['id', 'firstName', 'lastName', 'phoneNumber', 'email', 'instrument', 'parentname1', 'parentname2', 'parentPhoneNumber', 'parentEmail', 'address'],
-            // where: {
-            //     teacherId: req.user.id
-            // },
+            where: {
+                teacherId: req.user.id
+            },
             order: ['lastName']
         }).then((results) => {
             for (let i = 0; i < results.length; i++) {
                 students.push(results[i].dataValues);
             }
         }).then(function (results) {
-            console.log("rendering" + students);
             res.render('home', {
                 students: students,
                 page: 'home'
